62fd6697bea05fb3e2763c8e4f2a4b85bdddc657
Karim Moussa <karim.moussa@mail.mcgill.ca>
2023-01-26 21:24:36 -0500

    wrote createRequest method

--- src/DnsClient.java
+++ src/DnsClient.java
@@ -1,6 +1,12 @@
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,25 +25,31 @@ public class DnsClient {
 
 	public static void main(String[] args) {
 		parse(args);
-
 		createRequest();
 	}
 
-	private static void createRequest() {
+	private static DatagramPacket createRequest() {
+
+		DatagramSocket clientSocket = null;
+
 		Random random = new Random();
-		short id = (short)random.nextInt(1 << 16);
-		System.out.println(id);
-		
+		short id = (short) random.nextInt(1 << 16);
 
 		byte[] idBytes = ByteBuffer.allocate(2).putShort(id).array();
-		
+
 		ByteArrayOutputStream stream = new ByteArrayOutputStream();
-		
-		byte [] bytes = {(byte)0x01,(byte) 0x00};
-		byte [] QDCount = {(byte)0x00,(byte) 0x00 };
-		byte [] ANCount = {(byte)0x00,(byte) 0x00 };
-		byte [] NSCount = {(byte)0x00,(byte) 0x00 };
-		byte [] ARCount = {(byte)0x00,(byte) 0x00 };
+
+		byte[] bytes = { (byte) 0x01, (byte) 0x00 };
+		byte[] QDCount = { (byte) 0x00, (byte) 0x00 };
+		byte[] ANCount = { (byte) 0x00, (byte) 0x00 };
+		byte[] NSCount = { (byte) 0x00, (byte) 0x00 };
+		byte[] ARCount = { (byte) 0x00, (byte) 0x00 };
+
+		byte[] qname = domainToBytes();
+
+		byte[] qtype = { (byte) 0x00, (byte) type };
+		byte[] qclass = { (byte) 0x00, (byte) 0x01 };
+
 		try {
 			stream.write(idBytes);
 			stream.write(bytes);
@@ -45,16 +57,84 @@ public class DnsClient {
 			stream.write(ANCount);
 			stream.write(NSCount);
 			stream.write(ARCount);
+			stream.write(qname);
+			stream.write(qtype);
+			stream.write(qclass);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		System.out.println(stream.toByteArray().length);
-		
+
+		byte[] data = stream.toByteArray();
+
+		for (int i = 0; i < retries; i++) {
+			try {
+				clientSocket = new DatagramSocket();
+				clientSocket.setSoTimeout(timeout * 1000);
+			} catch (SocketException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				long startTime = System.currentTimeMillis();
+				
+				InetAddress inetAddress = getInetAddress();
+				DatagramPacket sendPacket = new DatagramPacket(data, data.length, inetAddress, port);
+				clientSocket.send(sendPacket);
+				data = new byte[512];
+				DatagramPacket receivePacket = new DatagramPacket(data, data.length);
+				clientSocket.receive(receivePacket);
+				
+				long endTime = System.currentTimeMillis();
+				double interval = (endTime - startTime)/1000.0;
+				
+				System.out.println("Response received after " + interval + " seconds ( " + i + "retries )");
+				return receivePacket;
+			} catch (IOException e) {
+				e.printStackTrace();
+				if (i == retries - 1) {
+					System.out.println("Max number of retries: " + retries + " was exceeded");
+				}
+			}
+		}
+		return null;
+
 	}
-	
-	
+
+	private static InetAddress getInetAddress() {
+		InetAddress address = null;
+		String[] subStrings = ipAddress.split("\\.");
+
+
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+		for (String s : subStrings) {
+			stream.write(Integer.parseInt(s));
+		}
+
+		try {
+			address = InetAddress.getByAddress(stream.toByteArray());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
+		return address;
+	}
+
+	private static byte[] domainToBytes() {
+
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+		String[] labels = name.split("\\.");
+		for (String s : labels) {
+			stream.write(s.length());
+			byte[] byteArray = s.getBytes(StandardCharsets.US_ASCII);
+			for (byte b : byteArray) {
+				stream.write(b);
+			}
+		}
+
+		return stream.toByteArray();
+	}
 
 	private static void parse(String[] args) {
 		ArrayList<String> argList = new ArrayList<String>(Arrays.asList(args));
